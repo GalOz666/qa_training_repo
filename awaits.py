91d6e494ae52f13ec655a5626fce61aad7b11192
galoz <gal.oz@powtoon.com>
2021-02-02 15:13:08 +0200

    remove functional style await.py stuff

--- awaits.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-
-
-def wait_for_visibility_css(driver, time_sec, css):
-    return WebDriverWait(driver, time_sec).until(
-            expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, css)))
